31617247f0d573c53ab7fd29180eb5dcab6a8a5e
rk05091993 <roshanngp121@gmail.com>
2026-03-17 16:20:21 +0530

    First Commit

---
+++ pages/HomePage.ts
@@ -0,0 +1,99 @@
+import { Page,expect,Locator  } from "@playwright/test";
+
+export class HomePage{
+
+//Page
+    private readonly page:Page;
+//Locator
+
+private readonly lnkMyAccount:Locator;
+private readonly lnkRegister:Locator;
+private readonly linkLogin:Locator;
+private readonly txtSearchbox:Locator;
+private readonly btnSearch:Locator;
+
+
+
+//Constructors
+constructor(page:Page)
+{
+    this.page=page;
+    this.lnkMyAccount=page.locator('span:has-text("My Account")');
+    this.lnkRegister=page.locator("//ul[@class='dropdown-menu dropdown-menu-right']//a[normalize-space()='Register']");
+    this.linkLogin=page.locator('a').filter({ hasText: 'Login' }).first();
+    this.txtSearchbox=page.getByPlaceholder('Search');
+    this.btnSearch=page.locator('button.btn.btn-default.btn-lg');
+}
+
+
+
+//Action methods
+
+    // Check if HomePage exists
+    async isHomePageExists(){
+
+        let title:string = await this.page.title();
+        if(title)
+        {
+            return true;
+        }
+        return false;
+    }
+     
+     // Click "My Account" link
+    async clickMyAccount(){
+        try {
+            await this.lnkMyAccount.click();
+        } catch (error) {
+            console.log(`Exception occurred while clicking 'My Account': ${error}`);
+            throw error;//It will throw error in command line
+        }
+    }
+
+    // Click "Register" link
+    async clickRegister(){
+        try {
+            await this.lnkRegister.click();
+        } catch (error) {
+            console.log(`Exception occurred while clicking 'Register': ${error}`);
+            throw error;
+        }
+    }
+    
+    // Click "Login" link
+    async clickLogin(){
+        try {
+            await this.linkLogin.click();
+        } catch (error) {
+            console.log(`Exception occurred while clicking 'Login': ${error}`);
+            throw error;
+        }
+    }
+
+    // Enter product name in the search box
+    async enterProductName(pName: string){
+        try {
+            await this.txtSearchbox.fill(pName);
+        } catch (error) {
+            console.log(`Exception occurred while entering product name: ${error}`);
+            throw error;
+        }
+    }
+
+    // Click the search button
+    async clickSearch(){
+        try {
+            await this.btnSearch.click();
+        } catch (error) {
+            console.log(`Exception occurred while clicking 'Search': ${error}`);
+            throw error;
+        }
+    }
+    
+
+
+
+
+
+
+}
---
+++ test.config.ts
@@ -0,0 +1,17 @@
+export class TestConfig{
+
+//appUrl="http://localhost/opencart/upload/" //local application
+appUrl="https://naveenautomationlabs.com/opencart/index.php?route=common/home" //remote application
+//appUrl="https://tutorialsninja.com/demo/index.php?route=common/home" //remote application
+
+
+//valid login credentials- create your own login account
+email="roshanpce112@gmail.com"
+password="Roshan123"
+
+
+//product details
+productName="MacBook"
+productQuantity="2"
+totalPrice="$1,204.00"
+}
